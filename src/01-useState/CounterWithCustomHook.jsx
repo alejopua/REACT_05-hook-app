96f6eb5f9e2e485bd45ad5e759da86b674d57677
alejopua <alejopua@gmail.com>
2024-01-04 10:17:13 -0500

    [feat](components): exposing hook methods

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,16 +1,16 @@
 import { useCounter } from "../hooks/useCounter"
 
 export default function CounterWithCustomHook() {
-  const { counter } = useCounter();
+  const { counter, increment, reset, decrement } = useCounter();
   return (
     <>
       <h1>Counter With Hook: { counter } </h1>
       <hr />
 
       <div className="d-flex justify-content-center">
-        <button className="btn btn-primary">-1</button>
-        <button className="btn btn-primary">Reset</button>
-        <button className="btn btn-primary">+1</button>
+        <button className="btn btn-primary" onClick={decrement}>-1</button>
+        <button className="btn btn-primary" onClick={reset}>Reset</button>
+        <button className="btn btn-primary" onClick={increment}>+1</button>
       </div>
     
     </>
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -3,8 +3,23 @@ import { useState } from "react"
 export const useCounter = (initialValue = 10) => {
   const [ counter, setCounter ] = useState( initialValue );
 
+  const decrement = () => {
+    setCounter( counter - 1 );
+  }
+  
+  const increment = () => {
+    setCounter( counter + 1 );
+  }
+
+  const reset = () => {
+    setCounter( initialValue );
+  }
+
+
   return {
     counter,
-    setCounter,
+    decrement,
+    increment,
+    reset,
   }
 }
