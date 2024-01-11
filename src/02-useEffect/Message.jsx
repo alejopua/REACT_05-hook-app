8e31b2fa6b520eb2fa53cb280d9d7697ba2d6215
alejopua <alejopua@gmail.com>
2024-01-11 14:40:36 -0500

    [feat](hook): useEffect

---
+++ src/02-useEffect/Message.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+export const Message = () => {
+  const [coors, setCoors] = useState({x:0 , y:0})
+
+  useEffect(() => {
+
+    const onMouseMove = ({x, y}) => {
+      setCoors({x,y})
+    }
+
+    window.addEventListener('mousemove' , onMouseMove);
+  
+    return () => {
+      window.removeEventListener('mousemove' , onMouseMove);
+    }
+  }, [])
+  
+  return (
+    <>
+      <h3>User already exists</h3>
+      { JSON.stringify(coors) }
+    </>
+  )
+}
