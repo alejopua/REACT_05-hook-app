b2f1c77ebe104c2cd8f435236c86b30f69c5f90b
alejopua <alejopua@gmail.com>
2024-01-11 14:40:15 -0500

    [feat](components): form with custom hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterWithCustomHook />
+    <FormWithCustomHook />
   </React.StrictMode>,
 )
