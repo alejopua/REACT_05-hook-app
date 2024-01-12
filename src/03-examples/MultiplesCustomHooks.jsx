01b6d05eb60e4182a491e979f184b71b5a3991a7
alejopua <alejopua@gmail.com>
2024-01-12 17:40:36 -0500

    [feat](hook): useFetch custom hook continue

--- src/03-examples/MultiplesCustomHooks.jsx
+++ src/03-examples/MultiplesCustomHooks.jsx
@@ -1,14 +1,31 @@
 import { useFetch } from "../hooks/useFetch"
 
 export const MultiplesCustomHooks = () => {
-  const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
-  console.log(data, isLoading, hasError);
-
+  const { data, isLoading } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
+  // console.log(data, isLoading, hasError);
+  const { author, quote } = !!data && data[0];
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
       <hr />
-    
+      {
+        (isLoading)
+          ? (
+            <div className="alert alert-info text-center">
+              Loading...
+            </div>
+          )
+          : (
+            <blockquote className="blockquote text-end">
+              <p className="mb-1 fs-1 text text-light">{author}</p>
+              <footer className="blockquote-footer fs-5 text text-white-50">{quote}</footer>
+            </blockquote>
+          )
+      }    
+
+      <button className="btn btn-primary">
+        Next quote
+      </button>
     </>
   )
 }
