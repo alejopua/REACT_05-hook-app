ce66baf8f01e729f2fe2632eb34e1f56f4ba1066
alejopua <alejopua@gmail.com>
2024-02-21 11:37:50 -0500

    [feat](testing): Sec12 -Testing in TodoItem / test 1

---
+++ test/08-useReducer/TodoItem.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { TodoItem } from "../../src/08-useReducer/TodoItem";
+
+describe('Testing in <TodoItem />', () => {
+  const todo = {
+    id: 1,
+    desc: 'Aprender React',
+    done: false
+  }
+
+  const onDeleteTodoMock = jest.fn()
+  const onToggleTodoMock = jest.fn()
+
+  beforeEach( () => jest.clearAllMocks() )
+
+  test('should show the completed todo', () => {
+    
+    render(
+      <TodoItem 
+        todo={todo}
+        onDeleteTodo={onDeleteTodoMock}
+        onToggleTodo={onToggleTodoMock}
+      />
+    )
+
+    const spanElement = screen.getByLabelText('span')
+    expect( spanElement.className ).toBe('align-self-center ')
+
+  });
+});
