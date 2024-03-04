fbdc751995f335e6929debadd6956b34e62be1f6
alejopua <alejopua@gmail.com>
2024-03-04 16:04:04 -0500

    Update main.jsx to use MultiplesCustomHooksVersion2

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-// import { MultiplesCustomHooks } from './03-examples'
-// import { TodoApp } from './08-useReducer/TodoApp'
-// import { HomePage } from './09-useContext/HomePage'
-import { MainApp } from './09-useContext/MainApp'
+import { MultiplesCustomHooksVersion2 } from './03-examples/MultiplesCustomHooksVersion2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
-      <MainApp />
+      <MultiplesCustomHooksVersion2 />
     </React.StrictMode>
   </BrowserRouter>
 )
