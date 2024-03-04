54d8e0a4d21e7a75f018e6eda546701b5188a317
alejopua <alejopua@gmail.com>
2024-03-04 15:11:50 -0500

    Update LoadingMsg component styling

--- src/03-examples/LoadingMsg.jsx
+++ src/03-examples/LoadingMsg.jsx
@@ -3,8 +3,8 @@ export const LoadingMsg = () => {
   return (
     <>
       <div 
-        style={{ height: 200 }}
-        className="d-flex align-items-center justify-content-center alert alert-info"
+        style={{ height: 150 }}
+        className="alert alert-info p-0 d-flex align-items-center justify-content-center"
       >
         Loading...
       </div>
