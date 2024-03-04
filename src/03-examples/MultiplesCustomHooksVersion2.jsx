c5e88b6f59855d054648450fb181c19e15af3856
alejopua <alejopua@gmail.com>
2024-03-04 13:03:07 -0500

    Add MultiplesCustomHooksVersion2 component

---
+++ src/03-examples/MultiplesCustomHooksVersion2.jsx
@@ -0,0 +1,31 @@
+// import { useCounter } from "../hooks/useCounter";
+import { useCounter } from "../hooks/useCounter";
+import { useFetch } from "../hooks/useFetch"
+import { LoadingQuote, Quote } from ".";
+
+export const MultiplesCustomHooksVersion2 = () => {
+  const { increment } = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes`);
+  const { author, quote } = !!data && data[0];
+
+  return (
+    <>
+      <h1>Información de Pokémon</h1>
+      <hr />
+      
+      {
+        (isLoading)
+          ? <LoadingQuote />
+          : <Quote author={author} quote={quote}/>
+      }    
+
+      <button 
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={() => increment()}
+      >
+        Next quote
+      </button>
+    </>
+  )
+}
